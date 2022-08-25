fc79cb5162e42c9b31d1585492956fec937a9004
emindemirhan <emindemirhan@icloud.com>
2022-08-26 02:52:01 +0300

    navbar is complated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+import styles from "./style";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div className="bg-primary text-5xl w-full overflow-hidden">
+    <div className={`${styles.paddingX}  ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth} text-white`}>
+      <Navbar />
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+      <Hero />
+      </div>
+    </div>
+    <div className={`bg-primary  text-black ${styles.flexStart} ${styles.paddingX} `}>
+      <div className={`${styles.boxWidth}`}>
+       <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
 
-export default App
+export default App;
